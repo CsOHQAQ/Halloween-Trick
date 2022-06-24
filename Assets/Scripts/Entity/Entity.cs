17d073ec853c263dcd089addfa4cbd93a39a2e6d
呵呵笑的菊花QAQ <529353999@qq.com>
2022-06-24 21:14:35 +0800

    *Entity的基类属性确定

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -3,47 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using QxFramework.Core;
 
-public class Entity 
+public class Entity :MonoBehaviour
 {
-    public string Name;
+    public float curHealth;
+    public float MaxHealth;
+    public float MoveSpeed;
 
-    public List<string> character;
-    public List<string> translatedChara;
-    public string sex;
-    public string sexualOrientation;
+    private UIBase HealthSlideUI;
 
-    public string characterInfo;
-
-    public float timeLimit;
-    public float counter;
-    //头像地址
-    public string head;
-    //立绘地址
-    public string animation;
-
-    public bool isSpecial = false;
-
-    public List<string> likes;
-    public List<string> dislikes;
-
-
-
-    public int ID;
-
-    public void TagToInfo(int charaID)
+    public virtual void Init()
     {
-        TableAgent tab = new TableAgent();
-        tab.Add(ResourceManager.Instance.Load<TextAsset>("Text/Table/Tags").text);
-        foreach(var tag in character)
-        {
-            string[] description = tab.GetStrings("Tags", tag, "Description");
-            int desId = Random.Range(0, description.Length);
-            characterInfo += description[desId] + "\n";
-
-        }
 
     }
 
 
+    private void Update()
+    {
+        if (curHealth <= 0)
+        {
+            DestroyImmediate(this.gameObject);
 
+        }
+    }
+
+    
 }
--- Assets/Scripts/Entity/EntityManager.cs
+++ Assets/Scripts/Entity/EntityManager.cs
@@ -8,35 +8,14 @@ using UnityEngine.UI;
 
 public class EntityManager : MonoSingleton<EntityManager>
 {
-    //随机生成人物的计时器
-    float counter;
-    //生成下一个人物所需时间
-    float spawnTime=5;
-    //每日收工时间
-    int endtime=21;
-    public  List<Entity> entityList;
-    public Dictionary<int, Entity> entityDic;
-
-    public float baseValue=5;//配对的基础价值
-    public float matchSuccessValue = 5;//tag匹配的增加价值
-	public const int minCount=3;
-    public const int maxCount=8;
-
-    public const float LifeTime = 90;
-
-    private Dictionary<string, GameDateTime> specialDic;
-
-
 
     private void Awake()
     {
-        entityList = new List<Entity>();
-        entityDic = new Dictionary<int, Entity>();
 
-        specialDic = new Dictionary<string, GameDateTime>();
     }
     private void Update()
     {
+
     }
 
 }
